1a76dcfab57806c8d243bc93386858f8da65a9cc
lwugang <13480020053@163.com>
2019-11-04 14:20:14 +0800

    init

--- eastnotes/settings/local.py
+++ eastnotes/settings/local.py
@@ -4,16 +4,19 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'djangoblog',
-    #     'USER': 'root',
-    #     'PASSWORD': 'root',
-    #     'PORT': '3306',
-    #     'HOST': 'localhost',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MyBlog',
+        'USER': 'root',
+        'PASSWORD': 'lwg123456',
+        'HOST': '123.206.218.63',
+        'CHARSET': 'utf8',
+        'COLLATION': 'utf8',
+        'PORT': 3306,
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
